2b39501e9d1c09e2ae6f075954585c01bdf96159
CaydnPB <117025523+CaydnPB@users.noreply.github.com>
2023-12-19 14:14:37 +0000

    Add finishing touches to SprintBoard

--- SprintBoard/projectboard/forms.py
+++ SprintBoard/projectboard/forms.py
@@ -6,8 +6,20 @@ class SprintBoardIssueForm(forms.ModelForm):
     class Meta:
         model = SprintBoardIssue
         fields = ['title', 'description', 'project_number', 'issue_status']
+    
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['title'].widget.attrs.update({'class': 'form-control'})
+        self.fields['description'].widget.attrs.update({'class': 'form-control'})
+        self.fields['project_number'].widget.attrs.update({'class': 'form-control'})
+        self.fields['issue_status'].widget.attrs.update({'class': 'form-control'})
 
 class SprintBoardProjectForm(forms.ModelForm):
     class Meta:
         model = SprintBoardProject
         fields = ['title', 'description']
+    
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['title'].widget.attrs.update({'class': 'form-control'})
+        self.fields['description'].widget.attrs.update({'class': 'form-control'})
